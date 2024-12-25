28f72841ce0d23bd5b8119296b9fee8697201f53
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2024-12-25 13:03:33 -0800

    Update Game_Lists.lua

--- Game_Lists.lua
+++ Game_Lists.lua
@@ -1,5 +1,6 @@
 local games = {
-    [3101667897] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/LegendOfSpeed.lua" -- Legends of Speed
+    [3101667897] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/LegendOfSpeed.lua", -- Legends of Speed
+    [1962086868] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/TowerOfHell.lua", -- Tower of hell
 }
 
 return games
