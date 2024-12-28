33ae729ccd4ea73b106f7509fe467f94eff3ca44
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2024-12-28 11:58:02 -0800

    Update Game_Lists.lua

--- Game_Lists.lua
+++ Game_Lists.lua
@@ -2,6 +2,7 @@ local games = {
     [3101667897] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/LegendOfSpeed.lua", -- Legends of Speed
     [1962086868] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/TowerOfHell.lua", -- Tower of hell
     [14958096162] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/Evolve.lua", -- Evolve
+    [3956818381] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/ninjaLegends.lua", -- Ninja Legends
 }
 
 return games
