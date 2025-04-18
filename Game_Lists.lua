8d88a0e21bcaf30680d06cdb1cde5da08d0325bf
IsThisMe01 <150212080+IsThisMe01@users.noreply.github.com>
2025-04-18 12:48:47 -0700

    Update Game_Lists.lua

--- Game_Lists.lua
+++ Game_Lists.lua
@@ -4,7 +4,8 @@ local games = {
     [14958096162] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/Evolve.lua", -- Evolve
     [3956818381] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/ninjaLegends.lua", -- Ninja Legends
     [11445923563] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/oneFruitSim.lua", -- One fruit Simulator
-    [18901165922] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/PetGo.lua" -- pet go 
+    [18901165922] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/PetGo.lua", -- pet go 
+    [81440632616906] = "https://raw.githubusercontent.com/IsThisMe01/ProjectLV2/refs/heads/main/DigEarth.lua",
 }
 
 return games
